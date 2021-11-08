60403ded51030150cd01800c918a51f374b0cbf9
harshithaputtaswamy <harshi2680@gmail.com>
2021-11-09 02:03:54 +0530

    departments

--- contact_details/admin.py
+++ contact_details/admin.py
@@ -3,3 +3,4 @@ from .models import *
 
 # Register your models here.
 admin.site.register(user_detail)
+admin.site.register(departments)
--- contact_details/models.py
+++ contact_details/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+class departments(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 class user_detail(models.Model):
     role_choices = (
         ("admin", "Admin"),
@@ -13,11 +19,14 @@ class user_detail(models.Model):
         ("kitchen", "Kitchen"),
         ("goshala", "Goshala")
     )
-    email = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, blank=True, null=True)
     name = models.CharField(max_length=255, blank=True, null=True)
     phno = models.CharField(max_length=12)
     role = models.CharField(max_length=20, choices=role_choices, default="staff")
-    department = models.CharField(max_length=100, choices=department_choices, default="kitchen")
-
+    department = models.ForeignKey(departments, on_delete=models.SET_NULL, null=True )
+    
     class Meta:
         db_table = "User Detail"
+
+    def __str__(self):
+        return self.name
