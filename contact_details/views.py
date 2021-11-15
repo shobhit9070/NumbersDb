c22c62b765ed7c681fe057cb14f4f04f8d031699
harshithaputtaswamy <harshi2680@gmail.com>
2021-11-15 14:09:35 +0530

    datatable

--- contact_details/views.py
+++ contact_details/views.py
@@ -7,24 +7,43 @@ from django.http import HttpResponseRedirect
 import pandas as pd
 import openpyxl
 from io import BytesIO
-
+import re
 
 ##########GLOBAL VARS#########
 
 role_choices = {
     "admin": "admin",
     "department head": "dept_head",
-    "staff" : "staff"
+    "staff" : "staff",
+    "trustee": "trustee",
 }
 
+email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 ##########GLOBAL VARS END##########
 
 
 ##########HELPER FUNCTIONS START#################
 def validate_data_point(data):
     #If not valid return False,"error message"
+
+    if str(data[1])[-10:].isnumeric() != True:
+        print("return")
+        return False, "Invalid phone number"
+    # if not re.fullmatch(email_regex, data[2]):
+    #     return False, "Invalid email address"
+    if not data[3].lower().strip() in role_choices:
+        return False, "Invalid role - '" + data[3] + "'. Valid options are " + str(list(role_choices.keys()))
+    new_trust = None
+    if len(data) == 6 and not trust.objects.filter(name=data[5].lower().strip()).exists():
+        new_trust = trust(name=data[5].lower().strip())
+        new_trust.save()
+    if not department.objects.filter(name=data[4].lower().strip()).exists():
+        new_dept = department(name=data[4].lower().strip(), trust=new_trust)
+        new_dept.save()
     return True,"no error"
 
+def sanitize_phno(phno):
+    return str(phno)[-10:]
 
 #############HELPER FUNCTIONS END###################
 
@@ -77,17 +96,17 @@ def upload_bulk_contacts(request):
                     print(row_data)
                     is_valid, reason = validate_data_point(row_data)
                     if not is_valid:
-                        error_list.append((row_data,reason))
+                        error_list.append({'data': row_data, 'status': reason, "row_idx": str(row[0].row)})
                         continue
-                    elif user_detail.objects.filter(phno = row_data[1].split('.')[0]).exists():
+                    elif user_detail.objects.filter(phno = sanitize_phno(row_data[1].split('.')[0])).exists():
                         error_list.append({'data': row_data, 'status': "Entry with same phone number already exists", "row_idx": str(row[0].row)})
                     else:
-                        new_user_detail = user_detail(name=row_data[0],phno=row_data[1].split('.')[0],email=row_data[2])
-                        print("dept ",row_data[3].lower().strip())
-                        new_user_detail.department = department.objects.get(name=row_data[3].lower().strip())
-                        new_user_detail.role =  role_choices[row_data[4].lower().strip()]
+                        new_user_detail = user_detail(name=row_data[0],phno=sanitize_phno(row_data[1].split('.')[0]),email=row_data[2])
+                        print("dept ",row_data[4].lower().strip())
+                        new_user_detail.department = department.objects.get(name=row_data[4].lower().strip())
+                        new_user_detail.role =  role_choices[row_data[3].lower().strip()]
                         new_user_detail.save()
-                        success_list.append({'data': row_data, 'status': "Success", "row_idx": str(row[0].row)})
+                        success_list.append({'data': row_data, 'status': "Successfully added", "row_idx": str(row[0].row)})
             data = {
                 "status": 1,
                 "error_list": error_list,
