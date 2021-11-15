44a9f58ec360d5ad07c979b2b4cd347073e4a402
harshithaputtaswamy <harshi2680@gmail.com>
2021-11-15 12:49:46 +0530

    datatable

--- contact_details/views.py
+++ contact_details/views.py
@@ -31,21 +31,6 @@ def validate_data_point(data):
 
 
 # Create your views here.
-def homepage(request):
-    return render(request, 'home_view.html')
-
-def access_relation(user):
-    relation_rule = relation_table.objects.all().values()[0]
-    print(relation_rule)
-    if relation_rule[user.role] == 'self':
-        user_details = [user]
-    elif relation_rule[user.role] == 'dept_staff':
-        user_details = user_detail.objects.filter(~Q(role="admin"), department=user.department)
-    elif relation_rule[user.role] == 'dept_trust':
-        user_details = user_detail.objects.filter(~Q(role="admin"), department__in=department.objects.filter(trust=user.department.trust))
-    elif relation_rule[user.role] == 'all':
-        user_details = user_detail.objects.all()
-    return user_details
 
 def contact_list(request):
     if request.user.is_authenticated:
@@ -65,53 +50,58 @@ def contact_list(request):
             user_details = []
         data = {
             'user_details': user_details,
-            'role': user.role,
-            'dept': user.department.name,
+            'curr_user': user,
         }
         return render(request, 'contact_list.html', context=data)
     else:
-        return HttpResponseRedirect('/oauth/login/google-oauth2/?next=/contact_list')
+        return HttpResponseRedirect('/oauth/login/google-oauth2/?next=/')
 
 def upload_bulk_contacts(request):
-    if request.method != "POST" or not "fileInput" in request.FILES:
-        data = {
-            "status":0
-        }
+    if request.user.is_authenticated:
+        if request.method != "POST" or not "fileInput" in request.FILES:
+            data = {
+                "status":0
+            }
+        else:
+            wb = openpyxl.load_workbook(request.FILES["fileInput"])
+            print(wb.sheetnames)
+            error_list = []
+            success_list = []
+            for sheet in wb.sheetnames:
+                worksheet = wb[sheet]
+                for row in worksheet.iter_rows():
+                    print(str(row[0].row))
+                    row_data = list()
+                    for cell in row:
+                        row_data.append(str(cell.value))
+                    print(row_data)
+                    is_valid, reason = validate_data_point(row_data)
+                    if not is_valid:
+                        error_list.append((row_data,reason))
+                        continue
+                    elif user_detail.objects.filter(phno = row_data[1].split('.')[0]).exists():
+                        error_list.append({'data': row_data, 'status': "Entry with same phone number already exists", "row_idx": str(row[0].row)})
+                    else:
+                        new_user_detail = user_detail(name=row_data[0],phno=row_data[1].split('.')[0],email=row_data[2])
+                        print("dept ",row_data[3].lower().strip())
+                        new_user_detail.department = department.objects.get(name=row_data[3].lower().strip())
+                        new_user_detail.role =  role_choices[row_data[4].lower().strip()]
+                        new_user_detail.save()
+                        success_list.append({'data': row_data, 'status': "Success", "row_idx": str(row[0].row)})
+            data = {
+                "status": 1,
+                "error_list": error_list,
+                "success_list": success_list
+            }
+            print(data)
+        return render(request, 'contacts_upload_status.html', context=data)
     else:
-        wb = openpyxl.load_workbook(request.FILES["fileInput"])
-        print(wb.sheetnames)
-        error_list = []
-        success_list = []
-        for sheet in wb.sheetnames:
-            worksheet = wb[sheet]
-            for row in worksheet.iter_rows():
-                print(str(row[0].row))
-                row_data = list()
-                for cell in row:
-                    row_data.append(str(cell.value))
-                print(row_data)
-                is_valid, reason = validate_data_point(row_data)
-                if not is_valid:
-                    error_list.append((row_data,reason))
-                    continue
-                elif user_detail.objects.filter(phno = row_data[1].split('.')[0]).exists():
-                    error_list.append({'data': row_data, 'status': "Entry with same phone number already exists", "row_idx": str(row[0].row)})
-                else:
-                    new_user_detail = user_detail(name=row_data[0],phno=row_data[1].split('.')[0],email=row_data[2])
-                    print("dept ",row_data[3].lower().strip())
-                    new_user_detail.department = department.objects.get(name=row_data[3].lower().strip())
-                    new_user_detail.role =  role_choices[row_data[4].lower().strip()]
-                    new_user_detail.save()
-                    success_list.append({'data': row_data, 'status': "Success", "row_idx": str(row[0].row)})
-        data = {
-            "status": 1,
-            "error_list": error_list,
-            "success_list": success_list
-        }
-        print(data)
-    return render(request, 'contacts_upload_status.html', context=data)
+        return HttpResponseRedirect('/oauth/login/google-oauth2/?next=/')
 
 
 def auth_logout(request):
+    print("logout", request.user.is_authenticated)
     logout(request)
+    print("logout", request.user.is_authenticated)
+
     return HttpResponseRedirect("/")
